8587057c0d6b3c919a6904c27c5bafe0f3745920
DumontDigital <zorfical@gmail.com>
2026-05-15 18:19:53 -0400

    Fix schedule photo upload booking flow

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ function sendHtmlWithGooglePlacesKey(res, fileName) {
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 
 // Cache-busting middleware for HTML files
 app.use((req, res, next) => {
